521d43abeb76b3cee22fe58fbf9442a0aa64c312
saga14YT <120998058+saga14YT@users.noreply.github.com>
2026-09-07 23:56:24 -0600

    feat: implementation of connection and automatic database initialization

--- database.py
+++ database.py
@@ -2,6 +2,7 @@ import os
 import mysql.connector
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
 def get_database_connection():
@@ -10,8 +11,40 @@ def get_database_connection():
         host=os.getenv("DB_HOST"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
+        database=os.getenv("DB_NAME"),
+        port=os.getenv("DB_PORT")
       )
+      return connection
     except mysql.connector.Error as err:
         print(f"Error: {err}")
         return None
+
+def star_init():
+    connect = get_database_connection()
+    if connect is None:
+        print("The connection could not be established.")
+        return
+        
+    cursor = connect.cursor()
+    
+    try:
+        with open("sql/init.sql", "r", encoding="utf-8") as file:
+            sql_script = file.read()
+
+       
+        cursor.execute(sql_script)
+
+        while cursor.nextset():
+            pass
+
+        connect.commit()
+        print("Database successfully initialized")
+        
+    except mysql.connector.Error as err:
+        print(f"Error starting the database: {err}")
+    finally:
+        cursor.close()
+        connect.close()
+
+if __name__ == "__main__":
+    star_init()
